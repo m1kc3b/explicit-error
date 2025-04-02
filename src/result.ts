d40b3d908919093efef6dc8674bb84034e36b04b
m1kc3b <m1kc3b@gmail.com>
2025-04-02 14:05:20 +0200

    impl isOkAnd, isErrAnd, map, mapOr, mapErr & tests

--- src/result.ts
+++ src/result.ts
@@ -1,53 +1,74 @@
-export type Result<T, E> = Ok<T> | Err<E>;
-
-export class Ok<T> {
-  constructor(public value: T) {}
-
-  isOk(): this is Ok<T> {
-    return true;
-  }
-
-  isErr(): this is Err<never> {
-    return false;
-  }
-
-  unwrap(): T {
-    return this.value;
-  }
-
-  unwrapOr(defaultValue: T): T {
-    return this.value;
-  }
-
-  map<U>(fn: (value: T) => U): Result<U, never> {
-    return new Ok(fn(this.value));
-  }
+export enum ResultKind {
+  Ok = "Ok",
+  Err = "Err",
 }
 
-export class Err<E> {
-  constructor(public error: E) {}
+export class Result<T, E> {
+  private constructor(private kind: ResultKind, private value?: T, private error?: E) {}
 
-  isOk(): this is Ok<never> {
-    return false;
+  static Ok<T, E>(value: T): Result<T, E> {
+    return new Result(ResultKind.Ok, value);
   }
 
-  isErr(): this is Err<E> {
-    return true;
+  static Err<T, E>(error: E): Result<T, E> {
+    return new Result(ResultKind.Err, undefined as never, error);
   }
 
-  unwrap(): never {
+  // Return true if the result is Ok
+  isOk(): boolean {
+    return this.kind === ResultKind.Ok;
+  }
+
+  // Return true if the result is Ok and the value matches the predicate
+  isOkAnd(predicate: T): boolean {
+    return this.kind === ResultKind.Ok && this.value === predicate;
+  }
+
+  // Return true if the result is Err
+  isErr(): boolean {
+    return this.kind === ResultKind.Err;
+  }
+
+  // Return true if the result is Err and the error matches the predicate
+  isErrAnd(predicate: E): boolean {
+    return this.kind === ResultKind.Err && this.error === predicate;
+  }
+
+  // Map the value if it's Ok, otherwise return the Err
+  map(fn: (value: T) => T): Result<T, E> {
+    if (this.kind === ResultKind.Ok) {
+      return Result.Ok(fn(this.value as T));
+    }
+    return this;
+  }
+
+  // Map the value if it's Ok, otherwise return a default value
+  map_or(fn: (value: T) => T, defaultValue: T): T {
+    if (this.kind === ResultKind.Ok) {
+      return fn(this.value as T);
+    }
+    return defaultValue;
+  }
+
+  // Map the error if it's Err, otherwise return the Ok
+  mapErr(fn: (error: E) => E): Result<T, E> {
+    if (this.kind === ResultKind.Err) {
+      return Result.Err(fn(this.error as E));
+    }
+    return this;
+  }
+
+  // Unwrap the value if it's Ok, otherwise throw an error
+  unwrap(): T {
+    if (this.kind === ResultKind.Ok) {
+      return this.value as T;
+    }
+
     throw new Error(`Tried to unwrap Err: ${this.error}`);
   }
 
-  unwrapOr<T>(defaultValue: T): T {
-    return defaultValue;
+  // Unwrap the value if it's Ok, otherwise return a default value
+  unwrapOrDefault(defaultValue: T): T {
+    return this.kind === ResultKind.Ok ? (this.value as T) : defaultValue;
   }
-}
-
-export function ok<T>(value: T): Result<T, never> {
-  return new Ok(value);
-}
-
-export function err<E>(error: E): Result<never, E> {
-  return new Err(error);
 }
